f63b7518fb82f7dbac09d76ddc824b1b1483d1c2
Aleksandr Fokin <aleksandr.fokin@gmail.com>
2017-06-25 23:37:59 +0300

    Environment Setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    path.resolve(__dirname, 'src/index') // our application's actual entry point, has to be last
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // enables to hot-replace modules without a full browser refresh
+    new webpack.NoErrorsPlugin() // keeps errors from breaking our hot reloading experience
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']}, // we could tell webpack to handle SaaS and LESS files here also
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'}, // the following 4 instructions are necessary for the file types Bootstrap utilizes for fonts
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
